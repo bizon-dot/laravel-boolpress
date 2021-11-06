079b737c79f2e5814dd06b647880472264a40d11
bizoon <r.aureli@protonmail.com>
2021-11-06 14:54:05 +0000

    Dashboard admin view

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    @include('admin.partials.head')
+
+    <title>@yield('title')</title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+</head>
+
+<body id="page-top">
+
+<!-- Page Wrapper -->
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    @include('admin.partials.navbar')
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+
+
+            <!-- Begin Page Content -->
+            @yield('content')
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+
+        
+        <!-- Footer -->
+        @include('admin.partials.footer')
+        <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+
+
+    @include('admin.partials.footer-script')
+
+</body>
+
+</html>
