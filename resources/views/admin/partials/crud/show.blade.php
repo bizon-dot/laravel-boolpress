6d51a9e8de7c6cf589f4dc3e2f8c87d509a2bb38
bizoon <r.aureli@protonmail.com>
2021-11-06 18:55:04 +0000

    Create()

---
+++ resources/views/admin/partials/crud/show.blade.php
@@ -0,0 +1,52 @@
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-striped ">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Content</th>
+                        <th scope="col">Slug</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                {{-- @foreach ($comics as $comic)
+                <tr>
+                    <th scope="row">{{$comic["id"]}}</th>
+                    <th scope="row">
+                        <a href="{{route("comics.show", $comic["id"])}}">
+                            {{$comic["title"]}}
+                        </a>
+                    </th>
+                    <th scope="row">{{$comic["description"]}}</th>
+                    <th scope="row">{{$comic["price"]}}</th>
+                    <th scope="row">{{$comic["series"]}}</th>
+                    <th scope="row">{{$comic["type"]}}</th>
+                    <th scope="row">{{$comic["sale_date"]}}</th>
+                    <td>
+                       
+                        <a href="{{route("comics.show", $comic["id"])}}">
+                            <i class="far fa-eye"></i>
+                        </a>
+                        <a>
+                            <i class="fas fa-edit"></i>
+                        </a>
+
+                        <form method="POST" action="{{ route('comics.destroy', $comic["id"]) }}">
+
+                            @csrf
+
+                            <input name="_method" type="hidden" value="DELETE">
+
+                            <button type="submit" class="btn btn-xs btn-danger btn-flat show_confirm" data-toggle="tooltip" title='Delete'>Delete</button>
+
+                        </form>
+
+                    </td>
+                </tr>
+
+                @endforeach --}}
+            </table>
+        </div>
+    </div>
